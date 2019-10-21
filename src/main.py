1af323a863584ee991aa0ccebcc717d94f236777
smn_crm <simon.cramer@hhu.de>
2019-10-21 02:03:05 +0200

    Clean up structure
    Move motion detection to movemenent_service, Add sound dicts

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-from imutils.video import VideoStream
 import imutils
 import cv2
+import pyaudio
+import time
+import numpy as np
+from imutils.video import VideoStream
 from src import movement_service as ms
 from src import sound_service as ss
 from collections import deque
-# ------------------------------- audio -------------------------------
-import pyaudio
-import numpy as np
-import wave
-import time
 
+# ------------------------------- audio -------------------------------
 base = ss.create_sound("sounds/Motion_Base.wav")
 kick = ss.create_sound("sounds/Motion_Kick.wav")
 kick_add = ss.create_sound("sounds/Motion_KickAdd.wav")
@@ -24,7 +23,6 @@ sounds = {
     "add2": add2,
 }
 
-# instantiate PyAudio
 p = pyaudio.PyAudio()
 
 # movement values to modify the sound
@@ -67,7 +65,7 @@ def callback(in_data, frame_count, time_info, status):
     data = 2 * sounds["base"]["data"]
     data = data + 3 * add_data_all
     data = data + add_data_left
-    data = data + add_data_right
+    data = data + 0.7 * add_data_right
 
     if movement.all > 0.4:
         data = data + ss.calculate_weighted_segment(old_movement.all,
@@ -83,99 +81,58 @@ def callback(in_data, frame_count, time_info, status):
     return res_data, pyaudio.paContinue
 
 
-# open stream using callback
 stream = p.open(format=p.get_format_from_width(base["wave"].getsampwidth()),
                 channels=base["wave"].getnchannels(),
                 rate=base["wave"].getframerate(),
                 output=True,
                 stream_callback=callback)
-
-# start the stream (4)
 stream.start_stream()
-# ----------------------------- end audio -----------------------------
 
-# ------------------------------- init --------------------------------
-min_area = 2000
-scaled_width = 700
+# ----------------------------- init video ----------------------------
+config_video = {
+    "frame width": 700,
+    "min area": 2000,
+    "frame area": 0,
+    "tracked frames": 50
+}
 
-# start webcam stream
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-firstFrame = None
-trackedFrames = 50
+frame = None
 frames = deque()
 
 # count the frames that have been processed since the firstFrame was updated
 count = 0
 # the movement value for every frame is stored in this list
 areas = deque()
-# ----------------------------- main loop -----------------------------
-while True:
-    # grab the current frame
+
+# make sure the frames deque is full before motion detection
+while count <= config_video["tracked frames"]:
     frame = vs.read()
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if frame is None:
-        break
-
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=scaled_width)
+    frame = imutils.resize(frame, width=config_video["frame width"])
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     # store the frame
     frames.append(gray)
 
-    # make sure the deque is filled first
-    if count <= trackedFrames:
-        frame_size = frame.shape[0] * frame.shape[1]
-        count = count + 1
-        continue
+    count = count + 1
 
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(frames.popleft(), gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+config_video["frame area"] = frame.shape[0] * frame.shape[1]
 
-    frameDelta_left = frameDelta[:, 0:(scaled_width // 2)].copy()
-    frameDelta_right = frameDelta[:, (scaled_width // 2):scaled_width].copy()
+# ----------------------------- main loop -----------------------------
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=config_video["frame width"])
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    frames.append(gray)
 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    thresh_left = thresh[:, 0:(scaled_width // 2)].copy()
-    thresh_right = thresh[:, (scaled_width // 2):scaled_width].copy()
-
-    cnts_left = cv2.findContours(thresh_left.copy(), cv2.RETR_EXTERNAL,
-                                 cv2.CHAIN_APPROX_SIMPLE)
-    cnts_right = cv2.findContours(thresh_right.copy(), cv2.RETR_EXTERNAL,
-                                  cv2.CHAIN_APPROX_SIMPLE)
-
-    cnts_left, cnts_right = imutils.grab_contours(cnts_left), \
-                            imutils.grab_contours(cnts_right)
-
-    movements = [0, 0]
-    for i in range(2):
-        cnts = [cnts_left, cnts_right][i]
-        area = 0
-        # loop over the contours
-        for c in cnts:
-            # if the contour is too small, ignore it
-            contour_area = cv2.contourArea(c)
-            if contour_area < min_area:
-                continue
-
-            # (x, y, w, h) = cv2.boundingRect(c)
-            # cv2.drawContours(frame, c, -1, (0, 255, 0), 2)
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            area = area + contour_area
-
-        movements[i] = round(area / frame_size, 4)
-
-    movement = ms.Movement(sum(movements), 2 * movements[0],
-                           2 * movements[1])
+    movement, frameDelta_left, frameDelta_right = ms.calculate_movement(frames,
+                                                                        config_video)
 
     # draw the text and timestamp on the frame
     cv2.putText(frame, "{}".format(movement.left), (10, 20),
@@ -192,25 +149,14 @@ while True:
 
     key = cv2.waitKey(1) & 0xFF
 
-    # if the `q` key is pressed, break from the loop
     if key == ord("q"):
         break
 
-# cleanup the camera and close any open windows
+# -------------------------- clean up ---------------------------
 vs.stop()
 cv2.destroyAllWindows()
-# --------------------------- end main loop ---------------------------
-
-# -------------------------- audio clean up ---------------------------
-# stop stream
 stream.stop_stream()
 stream.close()
-base.close()
-kick.close()
-kick_add.close()
-add1.close()
-add2.close()
-
-# close PyAudio
 p.terminate()
-# ------------------------ end audio clean up -------------------------
+for s in sounds:
+    sounds[s]["wave"].close()
--- src/movement_service.py
+++ src/movement_service.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+import cv2
+import imutils
 
 Movement = namedtuple('Movement', ['all', 'left', 'right'])
 
@@ -20,3 +22,51 @@ def process_new_movement(movement, old_movement):
         new_movement_right = movement.right
 
     return Movement(new_movement_all, new_movement_left, new_movement_right)
+
+
+def calculate_movement(frames, config):
+    # compute the absolute difference between the current frame and
+    # first frame
+    frame_delta = cv2.absdiff(frames.popleft(), frames[-1])
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    frame_delta_left = frame_delta[:, 0:(config["frame width"] // 2)].copy()
+    frame_delta_right = frame_delta[:, (config["frame width"] // 2):config[
+        "frame width"]].copy()
+
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    thresh_left = thresh[:, 0:(config["frame width"] // 2)].copy()
+    thresh_right = thresh[:,
+                   (config["frame width"] // 2):config["frame width"]].copy()
+
+    cnts_left = cv2.findContours(thresh_left.copy(), cv2.RETR_EXTERNAL,
+                                 cv2.CHAIN_APPROX_SIMPLE)
+    cnts_right = cv2.findContours(thresh_right.copy(), cv2.RETR_EXTERNAL,
+                                  cv2.CHAIN_APPROX_SIMPLE)
+
+    cnts_left, cnts_right = imutils.grab_contours(cnts_left), \
+                            imutils.grab_contours(cnts_right)
+
+    movements = [0, 0]
+    for i in range(2):
+        cnts = [cnts_left, cnts_right][i]
+        area = 0
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            contour_area = cv2.contourArea(c)
+            if contour_area < config["min area"]:
+                continue
+
+            # (x, y, w, h) = cv2.boundingRect(c)
+            # cv2.drawContours(frame, c, -1, (0, 255, 0), 2)
+            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            area = area + contour_area
+
+        movements[i] = round(area / config["frame area"], 4)
+
+    movement = Movement(sum(movements), 2 * movements[0], 2 * movements[1])
+
+    return movement, frame_delta_left, frame_delta_right
